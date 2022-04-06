a1be6a44cbeee884b219e6c9174a087860b418df
Rhys <rhyswoody@gmail.com>
2022-04-05 18:05:00 -0700

    complete exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :has_mens_andor_womens, on: :create
+
+  def has_mens_andor_womens
+    unless womens_apparel.present? || mens_apparel.present?
+      errors.add(:store, "store needs mens or womens apparel")
+    end
+  end
+
 end
